4836305906f12bd106c2dab0b10550bcb2317ed0
Bent4supper <ian.lemeur@epfl.ch>
2020-05-25 12:09:43 +0200

    update test vecteur

--- travail/testVecteur/testVecteur.cc
+++ travail/testVecteur/testVecteur.cc
@@ -8,20 +8,153 @@ using namespace std;
 int main(){
 
 //Initialisation================================================================================================
-    vector<double> v({1.0, 2.0, 4.1});
 
-    Vecteur vect1(1.0, 2.0, -0.1); // un vecteur en 3D :
-    Vecteur vect2(v); // un autre vecteur en 3D :
-    Vecteur vect3(vect1);  // copie de V1
-    Vecteur vect4(4);      // le vecteur nul en 4D
-    Vecteur vect5(1.,2.,3.), vect6(4.,5.,6.), vect7(7.,8.,9.), vect8({1.,3.});
+    // un vecteur en 3D :
+
+    Vecteur vect1(1.0, 2.0, -0.1);
+
+    // declaration d'un vector pour tester la  deuxieme methode de construction
+
+    vector<double> v({2.6, 3.5, 4.1});
+
+    // un autre vecteur en 3D :
+
+    Vecteur vect2(v);
+
+    // copie du Vecteur 1
+
+    Vecteur vect3(vect1);
+
+    // affichage des vecteurs
+
+    cout << "Affichage des Vecteurs : " << endl << endl;
+
+    cout << "Vecteur 1 : ";
+
+    vect1.affiche(cout) << endl;
+
+    // test de l'operateur <<
+
+    cout << "Vecteur 2 : ";
+
+    cout << vect2 << endl;
+
+    cout << "Vecteur 3 : ";
+
+    cout << vect3 << endl << endl;
+
+    // test de l'egalite de 2 vecteurs
+
+    cout << "Test de l'egalite de 2 Vecteurs : " << endl << endl;
+
+    cout << "Le Vecteur 1 est ";
+
+    // test de l'operateur ==
+
+    if (vect1==vect2) cout << "egal au";
+
+    else cout << "different du";
+
+    cout << " Vecteur 2," << endl << "et est ";
+
+    // test de l'operateur !=
+
+    if (vect1!=vect3) cout << "different du";
+
+    else cout << "egal au";
+
+    cout << " Vecteur 3." << endl << endl;
+
+    // test de l'operateur +
+
+    cout << "Test l'addition et sa commutativite : " << endl << endl;
+
+    cout << vect1 << " + " << vect2 << " = " << vect1+vect2 << endl;
+
+    //test la commutativite de l'addition
+
+    cout << vect2 << " + " << vect1 << " = " << vect2+vect1 << endl << endl;
+
+    // construction du vecteur nul de dimension 3 (constructeur par default)
+
+    cout << "Construction du Vecteur nul et test de ses proprietes : " << endl << endl;
+
+    Vecteur vect4;
+
+    cout << "Vecteur 4 : ";
+
+    cout << vect4 << endl;
+
+    // test l'element neutre pour l'addition
+
+    cout << vect1 << " + " << vect4 << " = " << vect1+vect4 << endl;
+
+    cout << vect4 << " + " << vect1 << " = " << vect4+vect1 << endl << endl;
+
+    // test de l'operateur -
+
+    cout << "Test de la soustraction : " << endl << endl;
+
+    cout << vect1 << " - " << vect2 << " = " << vect1-vect2 << endl;
+
+    // test si la soustraction d'un vecteur a lui meme retourne le vecteur nul
+
+    cout << vect2 << " - " << vect2 << " = " << vect2-vect2 << endl << endl;
+
+    // construction d'un vecteur de dimension 2
+
+    cout << "Construction d'un Vecteur de dimension 2 : " << endl << endl;
+
+    Vecteur vect5({1,2});
+
+    cout << "Vecteur 5 : ";
+
+    cout << vect5 << endl << endl;
+
+    // addition de deux vecteurs de dimensions differentes
+
+    cout << "Addition de deux Vecteurs de dimensions differentes : " << endl << endl;
+
+    try {
+        cout << vect2 << " + " << vect5 << " = " << vect2+vect5 << endl;
+    }
+
+    catch(int const& i){
+        switch(i){
+            case 1:cout << endl << endl << "Erreur: division par 0 impossible" << endl;
+            case 2:cout << endl << endl << "Erreur: dimension differente de 3" << endl;
+            case 3:cout << endl << endl << "Erreur: dimensions des vecteurs incompatibles" << endl;
+        }
+    }
+
+    try {
+        cout << vect5 << " + " << vect2 << " = " << vect5+vect2 << endl;
+    }
+
+    catch(int const& i){
+        switch(i){
+            case 1:cout << endl << endl << "Erreur: division par 0 impossible" << endl << endl;
+            case 2:cout << endl << endl << "Erreur: dimension differente de 3" << endl << endl;
+            case 3:cout << endl << endl << "Erreur: dimensions des vecteurs incompatibles" << endl << endl;
+        }
+    }
+
+    // test de l'opppose d'un vecteur
+
+    cout << "Test de l'opppose d'un Vecteur : " << endl << endl;
+
+    cout << "- " << vect1 << " = " << -vect1 << endl;
+
+    cout << "- " << vect1
+
+    //Vecteur vect4(4);      // le vecteur nul en 4D
+    Vecteur vect10(1.,2.,3.), vect6(4.,5.,6.), vect7(7.,8.,9.), vect8({1.,3.});
     Vecteur vect9({1,2}); //Vecteur 2D
 
 
 //Affichage des Vecteurs=========================================================================================
 
-    cout << "Vecteur 1 : ";
-    vect1.affiche(cout) << endl;
+
     cout << "Vecteur 2 : " << vect2 << endl;
     cout << "Vecteur 3 : " << vect3 << endl;
     cout << "Vecteur 4 : " << vect4 << endl;
